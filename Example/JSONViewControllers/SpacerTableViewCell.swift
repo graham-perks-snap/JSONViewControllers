b0f25f412b27bfc006de4c22ec9fc5fe465252a7
Graham Perks <graham_perks@snapkitchen.com>
2016-03-14 13:15:58 -0500

    Prevent spurious layout warnings

--- Example/JSONViewControllers/SpacerTableViewCell.swift
+++ Example/JSONViewControllers/SpacerTableViewCell.swift
@@ -13,19 +13,33 @@ import JSONViewControllers
 
 class SpacerTableViewCell: UITableViewCell, JSONTableCellConfigurer {
 
+    var heightConstraint: NSLayoutConstraint?
+
     func configureInTableViewController(tableViewController: UITableViewController, cellDefinition json: JSON) {
         backgroundColor = UIColor.clearColor()
         selectionStyle = .None
 
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+
+        // This is a simple row with no content; just a height.
         let height = CGFloat(json["height"].intValue)
-        var hConstraint: NSLayoutConstraint
-        if #available(iOS 9.0, *) {
-            hConstraint = contentView.heightAnchor.constraintEqualToConstant(height)
-        } else {
-            // Fallback on earlier versions
-            hConstraint = NSLayoutConstraint(item: contentView, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: height)
+        // Already got a constraint added to this row? (We must be re-using it)
+        if let c = heightConstraint {
+            // Update existing constraint
+            c.constant = height
         }
-        NSLayoutConstraint.activateConstraints([hConstraint])
+        else {
+            // Create new constraint.
+            var hConstraint: NSLayoutConstraint
+            if #available(iOS 9.0, *) {
+                hConstraint = contentView.heightAnchor.constraintEqualToConstant(height)
+            } else {
+                // Fallback on earlier versions
+                hConstraint = NSLayoutConstraint(item: contentView, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: height)
+            }
+            NSLayoutConstraint.activateConstraints([hConstraint])
+            heightConstraint = hConstraint
+        }
     }
 }
 
